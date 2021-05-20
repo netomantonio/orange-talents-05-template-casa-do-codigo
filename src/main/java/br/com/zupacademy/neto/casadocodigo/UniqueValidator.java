fd066efeaaf4f36d9d6036bba978ea6e8ce42bb2
[Neto] Antonio Miranda <netomantonio@gmail.com>
2021-05-19 23:25:33 -0300

    update livros

--- src/main/java/br/com/zupacademy/neto/casadocodigo/UniqueValidator.java
+++ src/main/java/br/com/zupacademy/neto/casadocodigo/UniqueValidator.java
@@ -1,7 +1,6 @@
 package br.com.zupacademy.neto.casadocodigo;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,16 +13,10 @@ import org.springframework.util.Assert;
 public class UniqueValidator implements ConstraintValidator<Unique, Object> {
 	private String domainAttribute;
 	private Class<?> klass;
-	
+
 	@PersistenceContext
 	private EntityManager manager;
-	
-//	@Autowired
-//	private AutorRepository autorRepository;
-//	
-//	@Autowired
-//	private CategoriaRepository categoriaRepository;
-	
+
 	@Override
 	public void initialize(Unique params) {
 		domainAttribute = params.fieldName();
@@ -38,8 +31,16 @@ public class UniqueValidator implements ConstraintValidator<Unique, Object> {
 		query.setParameter("value", value);
 		List<?> list = query.getResultList();
 		Assert.state(list.size() <= 1, "Foi encontrado mais de um "+klass+" com o atributo "+domainAttribute+" = "+value);
-		
+
 		return list.isEmpty();
 	}
-	
+//	@Override
+//	public boolean isValid(Object value, ConstraintValidatorContext context) {
+//		Query query = em.createQuery("SELECT 1 FROM  " + entidadeDominio.getName() + " WHERE "
+//				+ atributo + "=:value");
+//		query.setParameter("value", value);
+//		List<?> resultQuery = query.getResultList();
+//		boolean entidadeExistente = !resultQuery.isEmpty();
+//		return entidadeExistente;
+//	}
 }
--- src/main/java/br/com/zupacademy/neto/casadocodigo/autor/AutorRepository.java
+++ src/main/java/br/com/zupacademy/neto/casadocodigo/autor/AutorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AutorRepository extends JpaRepository<Autor, Integer> {
 	Optional<Autor> findByEmail(String email);
+	Optional<Autor> findById(Integer id);
 }
